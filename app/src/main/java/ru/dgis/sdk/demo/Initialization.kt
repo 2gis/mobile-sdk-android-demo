f5a9258af2e70477e543128904cb8d8e2a640af8
t.artikov <t.artikov@2gis.ru>
2021-05-18 16:37:24 +0700

    [SDK-762] FirebaseLogSink
    Записываем предупреждения и ошибки из лога SDK как нефатальные ошибки Firebase

--- app/src/main/java/ru/dgis/sdk/demo/Initialization.kt
+++ app/src/main/java/ru/dgis/sdk/demo/Initialization.kt
@@ -3,6 +3,8 @@ package ru.dgis.sdk.demo
 import ru.dgis.sdk.DGis
 import ru.dgis.sdk.context.ApiKeys
 import ru.dgis.sdk.context.Context
+import ru.dgis.sdk.context.LogLevel
+import ru.dgis.sdk.context.LogOptions
 
 fun initializeDGis(appContext: android.content.Context): Context {
     val key = { id: Int -> String
@@ -12,6 +14,10 @@ fun initializeDGis(appContext: android.content.Context): Context {
         appContext, ApiKeys(
             directory = key(R.string.dgis_directory_api_key),
             map = key(R.string.dgis_map_api_key)
+        ),
+        logOptions = LogOptions(
+            customLevel = LogLevel.WARNING,
+            customSink = createLogSink()
         )
     )
 }
