9e06c4893345fdd0d2dfd0e3b51a3ad2481c4424
Sameri11 <51940183+Sameri11@users.noreply.github.com>
2024-08-26 19:24:52 +0500

    [SDK-#] add comment about Android Auto

--- app/src/main/java/ru/dgis/sdk/demo/car/MapService.kt
+++ app/src/main/java/ru/dgis/sdk/demo/car/MapService.kt
@@ -11,6 +11,12 @@ class MapService : CarAppService() {
         return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
     }
 
+    // In this application ru.dgis.sdk.Context created inside Application's onCreate, hence
+    // ru.dgis.sdk.Context is always available here.
+    //
+    // If use-case demands lazy of delayed initialization of ru.dgis.sdk.Context, i.e.
+    // only when certain activity/fragment/etc opened, and it is not created in Application's onCreate,
+    // ru.dgis.sdk.Context should be created here in onCreateSession or somewhere earlier.
     override fun onCreateSession(): Session {
         return MapSession()
     }
