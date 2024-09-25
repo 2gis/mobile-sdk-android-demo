59bddf6e47be2cfb82d8e5d445dc7739cc19cc32
v.lazin <v.lazin@2gis.ru>
2024-09-25 18:36:30 +0600

    SDK 12.1.0
    [SDK-#] В пример Android Auto добавлен CustomRenderer для демонстрации отрисовки поверх карты

---
+++ app/src/main/java/ru/dgis/sdk/demo/car/CustomRenderer.kt
@@ -0,0 +1,82 @@
+package ru.dgis.sdk.demo.car
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.os.Build
+import android.view.Surface
+import androidx.annotation.RequiresApi
+
+/*
+ A sample renderer demonstrating drawing capabilities on a surface provided by AndroidAutoMapSession.
+ This renderer draws a circle with a black background and a number inside, which increments every second.
+ Refer to MapSession from this demo for details on how to use this renderer.
+ */
+class CustomRenderer {
+    private var renderThread: RenderThread? = null
+
+    fun start(surface: Surface) {
+        renderThread = RenderThread(surface)
+        renderThread?.start()
+    }
+
+    fun stop() {
+        renderThread?.interrupt()
+        renderThread?.join()
+        renderThread = null
+    }
+
+    private class RenderThread(private val surface: Surface) : Thread() {
+        private val renderIntervalMs = 1000L
+
+        @RequiresApi(Build.VERSION_CODES.M)
+        override fun run() {
+            var value = 0
+
+            while (!isInterrupted) {
+                var canvas: Canvas? = null
+                try {
+                    canvas = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        surface.lockHardwareCanvas()
+                    } else {
+                        surface.lockCanvas(null)
+                    }
+                    draw(value++, canvas)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    if (canvas != null) {
+                        surface.unlockCanvasAndPost(canvas)
+                    }
+                }
+
+                try {
+                    sleep(renderIntervalMs)
+                } catch (e: InterruptedException) {
+                    break
+                }
+            }
+        }
+
+        private fun draw(value: Int, canvas: Canvas) {
+            val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+                color = Color.BLACK
+                style = Paint.Style.FILL
+            }
+
+            // Draw a black circle
+            val centerX = 100f
+            val centerY = 100f
+            val radius = 50f
+            canvas.drawCircle(centerX, centerY, radius, paint)
+
+            // Draw the white text inside the circle
+            paint.apply {
+                color = Color.WHITE
+                textSize = 30f
+                textAlign = Paint.Align.CENTER
+            }
+            canvas.drawText("$value", centerX, centerY + paint.textSize / 3, paint)
+        }
+    }
+}
--- app/src/main/java/ru/dgis/sdk/demo/car/MapService.kt
+++ app/src/main/java/ru/dgis/sdk/demo/car/MapService.kt
@@ -3,8 +3,23 @@ package ru.dgis.sdk.demo.car
 import androidx.car.app.CarAppService
 import androidx.car.app.Session
 import androidx.car.app.validation.HostValidator
+import ru.dgis.sdk.DGis
+import ru.dgis.sdk.map.DgisSource.Companion.createDgisSource
+import ru.dgis.sdk.map.DgisSourceWorkingMode
+import ru.dgis.sdk.map.MapOptions
+import ru.dgis.sdk.map.MyLocationMapObjectSource
 
 class MapService : CarAppService() {
+
+    private fun createMapOptions(): MapOptions {
+        return MapOptions().apply {
+            sources = listOf(
+                createDgisSource(DGis.context(), DgisSourceWorkingMode.HYBRID_ONLINE_FIRST),
+                MyLocationMapObjectSource(DGis.context())
+            )
+        }
+    }
+
     override fun createHostValidator(): HostValidator {
         // Avoid using ALLOW_ALL_HOSTS_VALIDATOR in production as it is insecure.
         // Refer to the Android Auto documentation for proper security practices.
@@ -18,6 +33,6 @@ class MapService : CarAppService() {
     // only when certain activity/fragment/etc opened, and it is not created in Application's onCreate,
     // ru.dgis.sdk.Context should be created here in onCreateSession or somewhere earlier.
     override fun onCreateSession(): Session {
-        return MapSession()
+        return MapSession(createMapOptions())
     }
 }
